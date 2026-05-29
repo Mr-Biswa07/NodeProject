2f0930dd3d9a203dc332fc16c95ad8ef9ced3504
Mr-Biswa07 <bishwabhusan2004@gmail.com>
2026-05-29 17:46:12 +0530

    Update

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ const server = http.createServer((req, res) => {
 
 server.listen(3000, '0.0.0.0', () => {
 
-   console.log("Server running on port 3000");
+   console.log("Server Running on port 3000");
 
 });
